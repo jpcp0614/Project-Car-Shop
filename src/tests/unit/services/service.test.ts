1da82f89078a7445bdba9ed5fe7ed9616a12139d
jpcp0614 <jpcp0614@gmail.com>
2022-05-26 01:46:38 -0300

    :sparkles: feat: add comment route /cars

--- src/tests/unit/services/service.test.ts
+++ src/tests/unit/services/service.test.ts
@@ -3,7 +3,7 @@ import * as sinon from 'sinon';
 import CarService from '../../../services/CarService';
 import { carResolveMock } from '../mocks/carMock';
 
-describe('1 - Car Services Test', () => {
+describe('1 - Car Services Test in route /cars', () => {
   let carService = new CarService();
 
   describe('1.1 - "create" method:', () => {
