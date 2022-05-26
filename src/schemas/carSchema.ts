06822079d78fed3b8b2871225335684a5176b05a
jpcp0614 <jpcp0614@gmail.com>
2022-05-25 23:36:15 -0300

    :sparkles: feat: add versionKey = false

--- src/schemas/carSchema.ts
+++ src/schemas/carSchema.ts
@@ -11,6 +11,8 @@ export const carSchema = new Schema<CarDocument>({
   buyValue: { type: Number, required: true },
   doorsQty: { type: Number, required: true },
   seatsQty: { type: Number, required: true },
+}, {
+  versionKey: false,
 });
 
 // consulta em Course: // consulta em Course Trybe: https://app.betrybe.com/course/back-end/mongodb-com-nodejs-e-poo/mongodb-e-poo/91006798-2877-4004-9cf5-d2d72a859272/conteudos/36237ca5-7ba2-40e3-9cb7-5af2159d3bef/models/563b39d4-a90c-40c7-8f69-cc019c8ad1e8?use_case=side_bar
