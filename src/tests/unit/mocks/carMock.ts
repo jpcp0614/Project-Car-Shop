55154fe506c49df4646cd9ecfe1d475a4dd7243c
jpcp0614 <jpcp0614@gmail.com>
2022-05-26 01:45:58 -0300

    :sparkles: feat: add carCreateMock

--- src/tests/unit/mocks/carMock.ts
+++ src/tests/unit/mocks/carMock.ts
@@ -1,3 +1,12 @@
+export const carCreateMock = {
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
 export const carResolveMock = {
   _id: '4edd40c86762e0fb12000003',
   model: "Ferrari Maranello",
