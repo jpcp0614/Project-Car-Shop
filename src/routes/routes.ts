85ae07f2fc6a4016482f07b2e531abd0e6e6ea32
jpcp0614 <jpcp0614@gmail.com>
2022-05-25 23:17:45 -0300

    :sparkles: feat: addRouter

---
+++ src/routes/routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import MongoController from '../controllers/MongoController';
+
+class CustomRouter<T> {
+  public router: Router;
+
+  constructor() {
+    this.router = Router();
+  }
+
+  public addRouter = (
+    controller: MongoController<T>,
+    route: string = controller.route,
+  ) => {
+    this.router.post(route, controller.create);
+  };
+}
+
+export default CustomRouter;
+
+// consulta em Course: https://app.betrybe.com/course/back-end/mongodb-com-nodejs-e-poo/mongodb-e-poo/91006798-2877-4004-9cf5-d2d72a859272/conteudos/36237ca5-7ba2-40e3-9cb7-5af2159d3bef/routes/c25fcafa-eade-47dc-8101-bf255755e9c7?use_case=side_bar
