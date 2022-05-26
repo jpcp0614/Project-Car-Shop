acd8693816c2dbb1c6a83e59642d73d695431c47
jpcp0614 <jpcp0614@gmail.com>
2022-05-26 00:37:00 -0300

    :truck: chore: rename carMock to carCreateMock

--- src/tests/unit/models/model.test.ts
+++ src/tests/unit/models/model.test.ts
@@ -2,25 +2,25 @@ import { expect } from 'chai';
 import mongoose from 'mongoose';
 import Sinon from 'sinon';
 import CarModel from '../../../models/CarModel';
-import { carMock } from './carMock';
+import { carCreateMock } from '../mocks/carMock';
 
 describe('1 - Car Models Test', () => {
   let carModel = new CarModel();
 
   describe('1.1 - "create" method:', () => {
     before(() => {
-      Sinon.stub(carModel.model, 'create').resolves(carMock);
+      Sinon.stub(carModel.model, 'create').resolves(carCreateMock);
     });
 
     after(() => {
       Sinon.restore();
     });
 
-    it('a) should return a Car object with the "_id" property', async () => {
-      const carCreated = await carModel.create(carMock);
+    it('a) should return a Car object with the "_id" property\n', async () => {
+      const carCreated = await carModel.create(carCreateMock);
       expect(carCreated).to.be.an('object');
       expect(carCreated).to.have.property('_id');
-      expect(carCreated).to.deep.equal(carMock);
+      expect(carCreated).to.deep.equal(carCreateMock);
     })
   });
 });
