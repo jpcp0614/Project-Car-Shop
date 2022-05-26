22ea6266ab0d0ff30ab3a5d79ff910abefe33b7a
jpcp0614 <jpcp0614@gmail.com>
2022-05-26 03:22:45 -0300

    :heavy_check_mark: feat: req. 10 - OK

--- src/tests/unit/mocks/carMock.ts
+++ src/tests/unit/mocks/carMock.ts
@@ -16,3 +16,13 @@ export const carResolveMock = {
   seatsQty: 2,
   doorsQty: 2
 };
+
+export const carFindResolveMock = [{
+  _id: '4edd40c86762e0fb12000003',
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2
+}];
--- src/tests/unit/models/model.test.ts
+++ src/tests/unit/models/model.test.ts
@@ -1,12 +1,12 @@
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import CarModel from '../../../models/CarModel';
-import { carResolveMock } from '../mocks/carMock';
+import { carResolveMock, carFindResolveMock } from '../mocks/carMock';
 
-describe('1 - Car Models Test in route /cars', () => {
+describe('1 - Car Models Test in route...', () => {
   let carModel = new CarModel();
 
-  describe('1.1 - "create" method:', () => {
+  describe('1.1 - method POST /cars:', () => {
     before(() => {
       sinon.stub(carModel.model, 'create').resolves(carResolveMock);
     });
@@ -15,11 +15,43 @@ describe('1 - Car Models Test in route /cars', () => {
       sinon.restore();
     });
 
-    it('a) should return a Car object with the "_id" property\n', async () => {
+    it('a) should return a Car object with the "_id" property', async () => {
       const carCreated = await carModel.create(carResolveMock);
       expect(carCreated).to.be.an('object');
       expect(carCreated).to.have.property('_id');
       expect(carCreated).to.deep.equal(carResolveMock);
     })
+  });
+
+  describe('1.2 - method GET /cars:', () => {
+    before(() => {
+      sinon.stub(carModel.model, 'find').resolves(carFindResolveMock as any[]);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('a) should return an array of Cars', async () => {
+      const carsFound = await carModel.read();
+      expect(carsFound).to.be.an('array');
+      expect(carsFound).to.deep.equal(carFindResolveMock);
+    });
+  });
+
+  describe('1.3 - method GET /cars/:id:', () => {
+    before(() => {
+      sinon.stub(carModel.model, 'findOne').resolves(carResolveMock as any);
+    });
+
+    after(() => {
+      sinon.restore();
+    });
+
+    it('a) should return a Car object', async () => {
+      const carFound = await carModel.readOne(carResolveMock._id);
+      expect(carFound).to.be.an('object');
+      expect(carFound).to.deep.equal(carResolveMock);
+    });
   });
 });
