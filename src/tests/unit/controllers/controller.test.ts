570324625d5e69fe3867e582fe862f4502f18917
jpcp0614 <jpcp0614@gmail.com>
2022-05-26 01:47:19 -0300

    :heavy_check_mark: feat: req. 07 - OK

---
+++ src/tests/unit/controllers/controller.test.ts
@@ -0,0 +1,60 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import chaiHttp = require('chai-http');
+import CarModel from '../../../models/CarModel';
+import server from '../../../server';
+import { carCreateMock, carResolveMock } from '../mocks/carMock';
+
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('1 - Car Controllers Test in route /cars', () => {
+  let chaiHttpResponse: Response;
+  let carModel = new CarModel();
+  let app = server.getApp();
+
+  describe('1.1 - "create" method:', () => {
+    before(async () => {
+      sinon.stub(carModel.model, 'create').resolves(carResolveMock);
+    });
+  
+    after(()=>{
+      sinon.restore();
+    });
+  
+    it('a) should return a Car object and status 201', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/cars')
+        .send(carCreateMock)
+        .then(res => {
+          expect(res.body).to.be.an('object');
+          expect(res).to.have.status(201);
+          expect(res.body).to.deep.equal(carResolveMock);
+          return res.body;
+        });
+    });
+
+    it('b) should return error with status 400 - model empty', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/cars')
+        .send({
+          year: 1963,
+          color: "red",
+          buyValue: 3500000,
+          seatsQty: 2,
+          doorsQty: 2
+        })
+        .then(res => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property('error');
+          return res.body;
+        });
+    });
+  });
+
+
+});
